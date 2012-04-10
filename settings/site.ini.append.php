42b8fbfb348c05bf3d9519fb25f9c96f5156df43
Michael <michael@thinkcreative.com>
2012-04-10 10:58:36 -0400

    add settings for permissions

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -3,6 +3,12 @@
 [DebugSettings]
 DebugOutput=enabled
 
+[FileSettings]
+TemporaryPermissions=0777
+StorageDirPermissions=0777
+StorageFilePermissions=0777
+LogFilePermissions=0777
+
 [TemplateSettings]
 ShowUsedTemplates=enabled
 
