ce4cb690907e3d32a9973ef8edb7c47b6982befb
Michael <michael@thinkcreative.com>
2012-04-10 10:04:27 -0400

    change "factbox" and "quote" alignment attribute to a select

---
+++ settings/ezoe_attributes.ini.append.php
@@ -0,0 +1,21 @@
+<?php /*
+
+[CustomAttribute_factbox_align]
+Name=Alignment
+Type=select
+Selection[]
+Selection[left]=Left
+Selection[right]=Right
+Default=right
+
+[CustomAttribute_quote_align]
+Name=Alignment
+Type=select
+Selection[]
+Selection[none]=None
+Selection[left]=Left
+Selection[center]=Center
+Selection[right]=Right
+Default=right
+
+*/ ?>
