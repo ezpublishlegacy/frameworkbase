241bd4951fb0ba2988ac0d75d749de2f6510e07f
Michael <michael@thinkcreative.com>
2012-04-10 10:01:30 -0400

    add initial settings for images

---
+++ settings/image.ini.append.php
@@ -0,0 +1,11 @@
+<?php /* #?ini charset="utf-8"?
+
+[FileSettings]
+DirPermissions=0770
+ImagePermissions=0770
+
+[ImageMagick]
+Filters[]=centerimg=-gravity center -crop %1x%2+0+0 +repage
+Filters[]=strip=-strip
+
+*/ ?>
