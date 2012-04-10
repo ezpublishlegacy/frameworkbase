da614ee88f50fa04b25509f1faa065922b932bc7
Michael <michael@thinkcreative.com>
2012-04-10 09:56:15 -0400

    add initial menu identifier list settings

---
+++ settings/menu.ini.append.php
@@ -0,0 +1,36 @@
+<?php /* #?ini charset="utf-8"?
+
+[MenuContentSettings]
+# Default setting as found in settings/menu.ini is commented out
+TopIdentifierList[]
+TopIdentifierList[]=folder
+TopIdentifierList[]=feedback_form
+TopIdentifierList[]=gallery
+TopIdentifierList[]=forum
+TopIdentifierList[]=documentation_page
+TopIdentifierList[]=forums
+TopIdentifierList[]=event_calendar
+TopIdentifierList[]=multicalendar
+TopIdentifierList[]=link
+TopIdentifierList[]=blog
+TopIdentifierList[]=frontpage
+
+# Default setting as found in settings/menu.ini is commented out
+LeftIdentifierList[]
+LeftIdentifierList[]=folder
+LeftIdentifierList[]=feedback_form
+LeftIdentifierList[]=gallery
+LeftIdentifierList[]=forum
+LeftIdentifierList[]=documentation_page
+LeftIdentifierList[]=forums
+LeftIdentifierList[]=event_calendar
+LeftIdentifierList[]=multicalendar
+LeftIdentifierList[]=link
+LeftIdentifierList[]=blog
+LeftIdentifierList[]=frontpage
+
+# Classes to use in extra menu (infobox)
+ExtraIdentifierList[]
+ExtraIdentifierList[]=infobox
+
+*/ ?>
